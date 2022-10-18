8a1abfe1c35a5cd9becfd850eaaa1db524e5bb7f
danieltolkachev <danieltolkachev1@gmail.com>
2022-10-18 21:54:48 +0200

    first commit

---
+++ src/test/java/de/htw/webtech/hello/webtech/HelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htw.webtech.hello.webtech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
