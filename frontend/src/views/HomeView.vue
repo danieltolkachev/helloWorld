a50ce7f037b320525eb5635420f72d0cb975dff8
Momo <momo.khezri@yahoo.com>
2023-01-08 21:52:34 +0100

    Changes by Daniel

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -6,12 +6,11 @@
 </template>
 
 <script>
-import HelloWorld from '@/components/HelloWorld'
+import HelloWorld from '@/components/HelloWorld.vue'
 export default {
   name: 'HomeView',
   components: {
     HelloWorld
-
   }
 }
 </script>
