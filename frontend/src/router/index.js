b661a0ee5cc6f72f0ef110028690558ac720cfb4
Momo <momo.khezri@yahoo.com>
2023-01-08 21:53:01 +0100

    .

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Task from '@/views/TaskView'
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: '/tasks',
+    name: 'tasks',
+    component: Task
   },
   {
     path: '/about',
